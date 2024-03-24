09f5a8f6158b4631d74838ed1f0795a1b7027585
Muhammad Aldi Surya Putra <aldysuryaputra@gmail.com>
2024-03-24 13:14:14 +0700

    student function except edit/update

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -26,7 +26,7 @@ class StudentController extends Controller
      */
     public function create()
     {
-        return view('student.create');
+        return view('student.registration');
     }
 
     /**
@@ -35,8 +35,18 @@ class StudentController extends Controller
     public function store(Request $request)
     {
         try{
+            $this->validate($request, [
+                'name' => 'required',
+                'nisn' => 'required|numeric',
+                'nik' => 'required|numeric|digits:16',
+                'date_of_birth' => 'required',
+                'gender' => 'required',
+                'religion' => 'required',
+                'address' => 'required',
+                'last_education' => 'required',
+                'phone_number' => 'required',
+            ]);
             $date_of_birth_student = Carbon::parse($request->date_of_birth);
-
             $student = Student::create([
                 'name' => $request->name,
                 'nisn' => $request->nisn,
@@ -47,56 +57,64 @@ class StudentController extends Controller
                 'address' => $request->address,
                 'last_education' => $request->last_education,
                 'phone_number' => $request->phone_number,
+                // user id must be logging in, jadi nanti ganti jadi user_id login
+                // 'user_id' => auth()->id(),
                 'user_id' => 1,
                 'verify_status' => false
             ]);
 
-            $mother = Family::create([
-                'name' => $request->name_ibu,
-                'nik' => $request->nik_ibu,
-                'date_of_birth' => $request->date_of_birth_ibu,
-                'gender' => $request->gender_ibu,
-                'religion' => $request->religion_ibu,
-                'address' => $request->address_ibu,
-                'last_education' => $request->last_education_ibu,
-                'phone_number' => $request->phone_number_ibu,
-                'working_as' => $request->working_as,
-                'income' => $request->income,
-                'parent_status' => 'ibu',
-                'student_id' => $student->id
-            ]);
-
-            $father = Family::create([
-                'name' => $request->name_ayah,
-                'nik' => $request->nik_ayah,
-                'date_of_birth' => $request->date_of_birth_ayah,
-                'gender' => $request->gender_ayah,
-                'religion' => $request->religion_ayah,
-                'address' => $request->address_ayah,
-                'last_education' => $request->last_education_ayah,
-                'phone_number' => $request->phone_number_ayah,
-                'working_as' => $request->working_as,
-                'income' => $request->income,
-                'parent_status' => 'ayah',
-                'student_id' => $student->id
-            ]);
-
-            // Should check if family type is wali so it must only wali
-            // And should check if there is only one parent, father or mother
-            $wali = Family::create([
-                'name' => $request->name_wali,
-                'nik' => $request->nik_wali,
-                'date_of_birth' => $request->date_of_birth_wali,
-                'gender' => $request->gender_wali,
-                'religion' => $request->religion_wali,
-                'address' => $request->address_wali,
-                'last_education' => $request->last_education_wali,
-                'phone_number' => $request->phone_number_wali,
-                'working_as' => $request->working_as,
-                'income' => $request->income,
-                'parent_status' => 'wali',
-                'student_id' => $student->id
-            ]);
+            if ($request->name_ibu){
+                $date_of_birth_ibu = Carbon::parse($request->date_of_birth_ibu);
+                $mother = Family::create([
+                    'name' => $request->name_ibu,
+                    'nik' => $request->nik_ibu,
+                    'date_of_birth' => $date_of_birth_ibu,
+                    'gender' => 'perempuan',
+                    'religion' => $request->religion_ibu,
+                    'address' => $request->address_ibu,
+                    'last_education' => $request->last_education_ibu,
+                    'phone_number' => $request->phone_number_ibu,
+                    'working_as' => $request->working_as_ibu,
+                    'income' => $request->income_ibu,
+                    'parent_status' => 'ibu',
+                    'student_id' => $student->id
+                ]);
+            }
+            if ($request->name_ayah){
+                $date_of_birth_ayah = Carbon::parse($request->date_of_birth_ayah);
+                $father = Family::create([
+                    'name' => $request->name_ayah,
+                    'nik' => $request->nik_ayah,
+                    'date_of_birth' => $date_of_birth_ayah,
+                    'gender' => $request->gender_ayah,
+                    'religion' => $request->religion_ayah,
+                    'address' => $request->address_ayah,
+                    'last_education' => $request->last_education_ayah,
+                    'phone_number' => $request->phone_number_ayah,
+                    'working_as' => $request->working_as_ayah,
+                    'income' => $request->income_ayah,
+                    'parent_status' => 'ayah',
+                    'student_id' => $student->id
+                ]);
+            }
+            if ($request->name_wali){
+                $date_of_birth_wali = Carbon::parse($request->date_of_birth_wali);
+                $wali = Family::create([
+                    'name' => $request->name_wali,
+                    'nik' => $request->nik_wali,
+                    'date_of_birth' => $date_of_birth_wali,
+                    'gender' => $request->gender_wali,
+                    'religion' => $request->religion_wali,
+                    'address' => $request->address_wali,
+                    'last_education' => $request->last_education_wali,
+                    'phone_number' => $request->phone_number_wali,
+                    'working_as' => $request->working_as_wali,
+                    'income' => $request->income_wali,
+                    'parent_status' => 'wali',
+                    'student_id' => $student->id
+                ]);
+            }
+            
 
             $uniqueId = Str::random(10);
             $registration = Registration::create([
@@ -105,9 +123,12 @@ class StudentController extends Controller
                 'status' => 'daftar'
             ]);
 
-            return view('xxx', ['student' => $student, 'registration'=>$registration]);
+
+
+            return redirect()->route('student.show', ['registration_uid' => $uniqueId]);
+            
         }catch(\Exception $e){
-            dd($e);
+            return redirect()->back()->withErrors(['error_message' => $e->getMessage()]);
         }
 
     }
@@ -115,15 +136,10 @@ class StudentController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show()
+    public function show($registration_uid)
     {
-        $user = User::with('student')->find(Auth::id());
-
-        if (!$user || !$user->student){
-            abort(404);
-        }
-
-        return view('xxx', ['student' => $user->student]);
+        $data = Registration::where('registration_uid', $registration_uid)->with('student')->with('student.families')->first();
+        return view('student.show', ['data' => $data]);
     }
 
     /**
@@ -177,4 +193,5 @@ class StudentController extends Controller
             return view('xxx')->with(['error' => 'there is an error']);
         }
     }
+
 }
